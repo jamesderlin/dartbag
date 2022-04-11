e7ecf3a8007c1a8f4e974adf714e7b4d9f2e42fc
James Lin <jamesdlin@gmail.com>
2022-04-11 03:29:36 -0700

    Add `tty_utils.dart` with `getTerminalSize` and `wordWrap` functions
    Add some functions to use with command-line programs, particularly
    for word-wrapping help test.

---
+++ lib/tty_utils.dart
@@ -0,0 +1,72 @@
+import 'dart:io' as io;
+
+import 'dart_utils.dart';
+
+/// Tries to get the size of the terminal.
+///
+/// Mimics the logic `shutil.get_terminal_size` from Python:
+/// * Returns the values of the `COLUMNS` and `LINES` environment variables if
+///   available. (However, these variables usually are unexported shell
+///   variables and therefore typically are *not* available.)
+/// * Otherwise queries the connected TTY.
+/// * Otherwise falls back to default values (80 columns by 24 lines).
+void getTerminalSize({
+  OutputParameter<int>? width,
+  OutputParameter<int>? height,
+}) {
+  width?.value = tryParseInt(io.Platform.environment['COLUMNS']) ??
+      (io.stdout.hasTerminal ? io.stdout.terminalColumns : 80);
+  height?.value = tryParseInt(io.Platform.environment['LINES']) ??
+      (io.stdout.hasTerminal ? io.stdout.terminalLines : 24);
+}
+
+/// Word wraps a string to a maximum line length.
+///
+/// Existing newlines in the original string will be preserved.  Lines longer
+/// than the maximum line length will be split at whitespace nearest to but
+/// not beyond the maximum line length.  If there is no preceding whitespace in
+/// the line, then the line will be split at the maximum line length.
+///
+/// [maxLineLength] must be positive.
+///
+/// [hangingIndent] specifies how many spaces to indent all but the first line.
+/// It must be non-negative and strictly less than [maxLineLength].
+String wordWrap(
+  String s,
+  int maxLineLength, {
+  int hangingIndent = 0,
+}) {
+  assert(maxLineLength > 0);
+  assert(hangingIndent >= 0);
+  assert(hangingIndent < maxLineLength);
+
+  var lines = s.split('\n');
+  var result = <String>[];
+
+  var wordBreakRegExp = RegExp(r'\s');
+  var hangingIndentString = ' ' * hangingIndent;
+
+  var i = 0;
+  var indentString = '';
+  var j = 0;
+  while (i < lines.length && j < 20) {
+    j += 1;
+    var line = '$indentString${lines[i]}';
+    indentString = hangingIndentString;
+
+    if (line.length <= maxLineLength) {
+      result.add(line);
+      i += 1;
+      continue;
+    }
+
+    var breakIndex = line.lastIndexOf(wordBreakRegExp, maxLineLength);
+    if (breakIndex < hangingIndent) {
+      breakIndex = maxLineLength;
+    }
+    result.add(line.substring(0, breakIndex).trimRight());
+    lines[i] = line.substring(breakIndex).trimLeft();
+  }
+
+  return result.join('\n');
+}
---
+++ test/tty_utils_test.dart
@@ -0,0 +1,104 @@
+@TestOn('vm')
+
+import 'package:dart_utils/tty_utils.dart';
+
+import 'package:test/test.dart';
+
+void main() {
+  group('wordWrap:', () {
+    test('Unindented', () {
+      expect(
+        wordWrap('The quick brown fox jumps over the lazy dog.', 10),
+        'The quick\n'
+        'brown fox\n'
+        'jumps over\n'
+        'the lazy\n'
+        'dog.',
+      );
+
+      expect(
+        wordWrap('Jackdaws\nlove\nmy big sphinx of quartz.', 10),
+        'Jackdaws\n'
+        'love\n'
+        'my big\n'
+        'sphinx of\n'
+        'quartz.',
+      );
+
+      expect(
+        wordWrap('abc', 1),
+        'a\n'
+        'b\n'
+        'c',
+      );
+
+      expect(
+        wordWrap('abcdefghijklmnopqrstuvwxyz', 10),
+        'abcdefghij\n'
+        'klmnopqrst\n'
+        'uvwxyz',
+      );
+
+      expect(
+        wordWrap('abc    defghijklmnopqrstuvwxyz', 10),
+        'abc\n'
+        'defghijklm\n'
+        'nopqrstuvw\n'
+        'xyz',
+      );
+    });
+
+    test('hangingIndent', () {
+      expect(
+        wordWrap(
+          'The quick brown fox jumps over the lazy dog.',
+          10,
+          hangingIndent: 2,
+        ),
+        'The quick\n'
+        '  brown\n'
+        '  fox\n'
+        '  jumps\n'
+        '  over the\n'
+        '  lazy\n'
+        '  dog.',
+      );
+
+      expect(
+        wordWrap(
+          'Jackdaws\nlove\nmy big sphinx of quartz.',
+          10,
+          hangingIndent: 2,
+        ),
+        'Jackdaws\n'
+        '  love\n'
+        '  my big\n'
+        '  sphinx\n'
+        '  of\n'
+        '  quartz.',
+      );
+
+      expect(
+        wordWrap('abcd', 2, hangingIndent: 1),
+        'ab\n'
+        ' c\n'
+        ' d',
+      );
+
+      expect(
+        wordWrap('abcdefghijklmnopqrstuvwxyz', 10, hangingIndent: 2),
+        'abcdefghij\n'
+        '  klmnopqr\n'
+        '  stuvwxyz',
+      );
+
+      expect(
+        wordWrap('abc    defghijklmnopqrstuvwxyz', 10, hangingIndent: 2),
+        'abc\n'
+        '  defghijk\n'
+        '  lmnopqrs\n'
+        '  tuvwxyz',
+      );
+    });
+  });
+}
